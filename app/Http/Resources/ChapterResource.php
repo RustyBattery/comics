0b67ee4f70fd3923bf6059f2d925244284246893
Adukova Margarita <adukovarita.hits21@gmail.com>
2023-06-20 15:23:50 +0300

    add prev and next chapters (get chapter info resource)

--- app/Http/Resources/ChapterResource.php
+++ app/Http/Resources/ChapterResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Chapter;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ChapterResource extends JsonResource
@@ -9,7 +10,7 @@ class ChapterResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
@@ -25,6 +26,8 @@ class ChapterResource extends JsonResource
             'rating' => null,
             "is_available" => $this->number > 2 ? false : true,
             "pages" => $this->number > 2 ? null : PageResource::collection($this->pages()->get()),
+            "prev_chapter_id" => Chapter::where('status', 'approved')->where('number', $this->number - 1)->first()->id ?? null,
+            "next_chapter_id" => Chapter::where('status', 'approved')->where('number', $this->number + 1)->first()->id ?? null,
         ];
     }
 }
